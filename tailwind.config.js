de761c88579bf64b3c34c39bf1f34db257498477
Damian Alanis <damian.alanis98@gmail.com>
2022-12-19 01:21:18 -0600

    Add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 const tailwindConfig = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
