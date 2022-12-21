c657f4051f67dbeb01dcd61b0972bc51c9d72cdc
Damian Alanis <damian.alanis98@gmail.com>
2022-12-20 23:49:28 -0600

    Add navigation for dynamic routes

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,11 +1,47 @@
-import { Container, Heading } from '@chakra-ui/react';
+import Link from 'next/link';
+import { 
+    List, 
+    Heading, 
+    Divider, 
+    ListItem, 
+    Container 
+} from '@chakra-ui/react';
 // Layout
 import { BaseLayout } from '../../layout/base/BaseLayout';
+
+const clients = [
+    {
+        id: 'dalanisr',
+        name: 'Damian Alanis Ramirez'
+    },
+    {
+        id: 'nalanisr',
+        name: 'Nancy Alanis Ramirez'
+    }
+];
 
 const ClientsPage: React.FC = () => (
     <BaseLayout>
         <Container className='p-4'>
-            <Heading textAlign='center'>Clients section</Heading>
+            <Heading textAlign='center' marginBottom='0.75rem'>Clients section</Heading>
+            <Divider/>
+            <Heading as='h4' textAlign='center' size='lg' marginTop='0.75rem'>List of clients</Heading>
+            <List>
+                {
+                    clients.map(client => (
+                    <ListItem key={client.id}>
+                        <Link 
+                            href = {{
+                                pathname: 'clients/[clientId]',
+                                query: { clientId: client.id }
+                            }}
+                        >
+                            { client.name }
+                        </Link>
+                    </ListItem>
+                    ))
+                }
+            </List>
         </Container>
     </BaseLayout>
 );
