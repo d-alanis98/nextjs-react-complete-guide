fca036ffc9b50290ed092ae28f3736ec30242946
Damian Alanis <damian.alanis98@gmail.com>
2022-12-20 23:58:45 -0600

    Navigate programatically with useRouter hook

--- pages/clients/[clientId]/index.tsx
+++ pages/clients/[clientId]/index.tsx
@@ -1,16 +1,42 @@
+import { useCallback } from 'react';
 import { useRouter } from 'next/router';
-import { Container, Heading } from '@chakra-ui/react';
+import { 
+    Button, 
+    Heading, 
+    Divider, 
+    Container 
+} from '@chakra-ui/react';
 // Layout
 import { BaseLayout } from '../../../layout/base/BaseLayout';
 
 
 const ClientDetails: React.FC = () => {
-    const { query: { clientId } } = useRouter();
+    const { 
+        query: { clientId },
+        push  
+    } = useRouter();
     
+    const navigateToClientProjects = useCallback(() => {
+        push({
+            pathname: '/clients/[clientId]/projects',
+            query: { clientId }
+        });
+    }, [push, clientId]);
+
     return (
         <BaseLayout>
-            <Container padding='4'>
-                <Heading textAlign='center'>Client { clientId }</Heading>
+            <Container 
+                padding = '4'
+                display = 'flex'
+                alignItems = 'center' 
+                flexDirection = 'column'
+                justifyContent = 'center'
+            >
+                <Heading textAlign='center' marginBottom='0.75rem'>Client { clientId }</Heading>
+                <Divider/>
+                <Button colorScheme='blue' marginTop='0.75rem' onClick={ navigateToClientProjects }>
+                    Navigate to client projects
+                </Button>
             </Container>
         </BaseLayout>
 
